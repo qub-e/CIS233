805426be7898d796685dbf5f8dcbdf5690668f14
qub-e <63983603+qub-e@users.noreply.github.com>
2023-06-29 14:40:40 -0700

    Fixed drop down nav and cleaned up code

--- Week 5/jscript.js
+++ Week 5/jscript.js
@@ -1,35 +1,49 @@
+var chipDiv, objFact, objPic;
+
+objPic = document.getElementById('shibuya');
+
 function setCity(id){
-    var chipDiv, objFact, objPic;
+
+
+    //Headache recieved here apparently the value of an option doesn't pass as an 
+    //int in the parameter of a function
+    id = parseInt(id);
 
     for(let i = 1; i <= 5; i++){
         let obj = document.getElementById(`city${i}`);
         obj.style.display = 'none';
     }
 
-    alert(id);
+    objPic.style.border ='none';
 
     switch(id){
         case 1: city = "Guadalajara"; 
         document.getElementById('city1').style.display = 'inline'; 
         document.getElementById('logo').src = "images/jalosLogo.png";
+        objPic = document.getElementById('jalos1');
         break;
         case 2: city = "Mesa"; 
         document.getElementById('city2').style.display = 'inline';
         document.getElementById('logo').src = "images/mesaLogo.png";
+        objPic = document.getElementById('mesa');
         break;
         case 3: city = "San Diego"; 
         document.getElementById('city3').style.display = 'inline';
         document.getElementById('logo').src = "images/sandiegoLogo.png";
+        objPic = document.getElementById('cali');
         break;
         case 4: city = "Waterford"; 
         document.getElementById('city4').style.display = 'inline';
         document.getElementById('logo').src = "images/waterfordLogo.png";
+        objPic = document.getElementById('waterford');
         break;
         default: city = "Shibuya";
          document.getElementById('city5').style.display = 'inline';
          document.getElementById('logo').src = "images/shibuyaLogo.png";
+         objPic = document.getElementById('shibuya');
         break;
     }
+
+    objPic.style.border = '5px white solid';
     getWeatherData();
 }
-
--- Week 5/weather.js
+++ Week 5/weather.js
@@ -13,7 +13,8 @@ function getWeatherData() {
             document.getElementById("description").textContent =
               data.weather[0].description;
               document.getElementById("weather_icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-
+              document.getElementById("lon").textContent = data.coord.lon;
+              document.getElementById("lat").textContent = data.coord.lat;
             // Show weather information
             document.getElementById("weather-info").style.display = "block";
           })
@@ -21,31 +22,6 @@ function getWeatherData() {
             console.log("Error occurred while fetching weather data:", error);
           });
 }
-
-// Function to get weather data from OpenWeather API
-function getWeatherMap() {
-
-  const apiKey = "d0de9e699e0a53d46aaf9dc18db5b62c";
-  const apiUrl = `http://maps.openweathermap.org/maps/2.0/weather/${op}/${z}/${x}/${y}?appid=${apiKey}`;
-  // Fetch weather data
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      // Update weather information on the page
-      document.getElementById("location").textContent = data.name;
-      document.getElementById("temperature").textContent = data.main.temp;
-      document.getElementById("description").textContent =
-        data.weather[0].description;
-        document.getElementById("weather_icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-
-      // Show weather information
-      document.getElementById("weather-info").style.display = "block";
-    })
-    .catch((error) => {
-      console.log("Error occurred while fetching weather data:", error);
-    });
-}
-
 
 
 
